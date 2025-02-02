bc69cc7b433fdff338197475c6e01e16a16fa96e
kozzztia <kozzztia@gmail.com>
2025-02-02 21:46:42 +0200

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Noto_Sans, VT323 } from "next/font/google";
+import  Header  from "@/components/header/page";
+import "./globals.css";
+
+
+const getNotoSans = Noto_Sans ({
+  variable: "--font-noto-sans",
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const getVT323 = VT323 ({
+  variable: "--font-vt323",
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Next App",
+  description: "description about next app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${getNotoSans.variable} ${getVT323.variable}`}>
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
