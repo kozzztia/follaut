4297b1ae44a09bd8940d9700b1b56a40f45ec364
kozzztia <kozzztia@gmail.com>
2025-02-13 19:51:29 +0200

    fix dinamic routes

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -8,13 +8,17 @@ type User = {
     name: string;
     login: string;
 };
+type FullUser = User & {
+    password: string;
+    role: string;
+}
 
 export async function GET(request: NextRequest) {
     try {
         const login = request.nextUrl.searchParams.get("login");
 
         if (login) {
-            const user: User[] = (await sql`SELECT id, name, login FROM users WHERE login = ${login}`) as User[];
+            const user: FullUser[] = (await sql`SELECT * FROM users WHERE login = ${login}`) as FullUser[];
             if (user.length === 0) {
                 return NextResponse.json({ error: "Пользователь не найден" }, { status: 404 });
             }
--- src/app/users/[login]/page.tsx
+++ src/app/users/[login]/page.tsx
@@ -1,30 +1,40 @@
-import React from 'react';
+'use client';
+import { usePathname } from 'next/navigation';
+import React, { useState } from 'react';
 
-type PageProps = {
-    params: { login: string };
+type FullUser = {
+    id: number;
+    name: string;
+    login: string;
+    role: string;
+    password: string;
+
 };
 
-const getUser = async (login: string) => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users?login=${login}`, { cache: 'no-store' });
-    if (!res.ok) throw new Error('Пользователь не найден');
-    return res.json();
-};
 
-const Page = async ({ params }: PageProps) => {
-    const { login } = params;
 
-    if (!login) {
-        return <h2>⚠️ Пользователь не найден</h2>;
+const Page = () => {
+    const login = usePathname().split("/")[2];
+    const [user, setUser] = useState<FullUser | null>(null);
+
+    React.useLayoutEffect(() => {
+        const fetchUser = async () => {
+            const response = await fetch(`/api/users?login=${login}`);
+            const data = await response.json();
+            setUser(data);
+        };
+        fetchUser();
+    }, [login]);
+    if(!user) {
+        return <p>Loading...</p>
     }
-
-    const user = await getUser(login);
 
     return (
         <div>
-            <h2>👤 Пользователь: {user.name}</h2>
-            <p>🔑 Логин: {user.login}</p>
-            <p>🆔 ID: {user.id}</p>
-            <p> Role{user.role}</p>
+            <h2>👤 Пользователь: {user?.name}</h2>
+            <p>🔑 Логин: {user?.login}</p>
+            <p>🆔 ID: {user?.id}</p>
+            <p> Role: {user?.role}</p>
         </div>
     );
 };
